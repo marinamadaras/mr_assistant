4a125e703af584acd0e1944eee55bd45480252e6
GitHub Action <action@github.com>
2024-12-21 14:24:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/marinamadaras/mrassistant/services/MyProjectService.kt
+++ src/main/kotlin/com/github/marinamadaras/mrassistant/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.marinamadaras.mrassistant.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.marinamadaras.mrassistant.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
